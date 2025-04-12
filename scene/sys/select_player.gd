dd5bd2e8e975d3c31d0ecd92e8e346d52b424c63
ryowu <ryowu@msn.com>
2025-04-12 21:31:27 +1000

    Add select charactors voices

--- scene/sys/select_player.gd
+++ scene/sys/select_player.gd
@@ -1,25 +1,31 @@
 extends Node2D
+
 @onready var yunfeng: Button = $Control/HBoxContainer/yunfeng
 @onready var xiaoai: Button = $Control/HBoxContainer/xiaoai
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var select_char_audio: AudioStreamPlayer2D = $select_char_audio
 @onready var lbl_description: Label = $description/Panel/lbl_description
+@onready var player_voice: AudioStreamPlayer2D = $player_voice
 
 var is_focused = false
+var yunfeng_voice_index := 0
+var xiaoai_voice_index := 0
 
-const yunfeng_description = "云峰（Yunfeng）
-空军中校，年龄30，作战经验丰富
-多次执行高危任务，冷静果断
-擅长指挥与战术规划，是团队核心
-内心坚毅，沉稳可靠，战友信赖的领袖
-誓为人类守护最后一道防线"
+const yunfeng_description = "云峰（Yunfeng）\n空军中校，年龄30，作战经验丰富\n多次执行高危任务，冷静果断\n擅长指挥与战术规划，是团队核心\n内心坚毅，沉稳可靠，战友信赖的领袖\n誓为人类守护最后一道防线"
 
-const xiaoai_description = "小爱（Xiao Ai）
-年仅十八岁的天才少女，空军少校
-拥有惊人反应力和直觉，战斗本能出众
-性格开朗，活泼可爱，却从不掉以轻心
-驾驶技术娴熟，常以不按常理出牌取胜
-在战火中绽放的青春之光"
+const xiaoai_description = "小爱（Xiao Ai）\n年仅十八岁的天才少女，空军少校\n拥有惊人反应力和直觉，战斗本能出众\n性格开朗，活泼可爱，却从不掉以轻心\n驾驶技术娴熟，常以不按常理出牌取胜\n在战火中绽放的青春之光"
+
+const yunfeng_voice = [
+	"res://assets/voice/select_chars/yunfeng_s1.mp3",
+	"res://assets/voice/select_chars/yunfeng_s2.mp3",
+	"res://assets/voice/select_chars/yunfeng_s3.mp3"
+]
+
+const xiaoai_voice = [
+	"res://assets/voice/select_chars/xiaoai_s1.mp3",
+	"res://assets/voice/select_chars/xiaoai_s2.mp3",
+	"res://assets/voice/select_chars/xiaoai_s3.mp3"
+]
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -39,14 +45,35 @@ func _on_xiaoai_pressed() -> void:
 
 
 func _on_yunfeng_focus_entered() -> void:
+	_play_next_voice(yunfeng_voice, "yunfeng")
 	if is_focused:
 		select_char_audio.play()
 		animation_player.play("change_char_to_yunfeng")
 		lbl_description.text = yunfeng_description
+		_play_next_voice(yunfeng_voice, "yunfeng")
 	else:
 		is_focused = true
+
 
 func _on_xiaoai_focus_entered() -> void:
 	select_char_audio.play()
 	animation_player.play_backwards("change_char_to_yunfeng")
 	lbl_description.text = xiaoai_description
+	_play_next_voice(xiaoai_voice, "xiaoai")
+
+func _play_next_voice(voice_array: Array, who: String) -> void:
+	await get_tree().create_timer(0.2).timeout
+	var index = 0
+	if who == "yunfeng":
+		index = yunfeng_voice_index
+		yunfeng_voice_index = (yunfeng_voice_index + 1) % voice_array.size()
+	elif who == "xiaoai":
+		index = xiaoai_voice_index
+		xiaoai_voice_index = (xiaoai_voice_index + 1) % voice_array.size()
+	
+	var path = voice_array[index]
+	var stream = load(path)
+	if stream:
+		player_voice.stop()
+		player_voice.stream = stream
+		player_voice.play()
